767dcdd539b6e637d0a27d122da233a12cecb8bd
DenisSamoilyk <deniskakbsamoilyk@gmail.com>
2023-10-21 21:50:27 +0300

    homework

---
+++ Homework 4.py
@@ -0,0 +1,16 @@
+word = "123456789 hello"
+# print(word[2])
+# before_last = (word[-2])
+# print(before_last[0])
+# print(word[0:5])
+# print(word[:-2])
+# print(word[1::2])
+# print(word[0::2])
+# print(word[::-1])
+# print(word[::-2])
+# print(len(word))
+
+print(word[:-1].count(" ")+1)
+
+
+
